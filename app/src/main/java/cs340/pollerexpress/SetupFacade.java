9c4c3a9ae718c3504ab67eb219f9b1c5410d438c
jackson <2jacksonhunt@gmail.com>
2018-09-30 10:44:03 -0600

    added an exclude from the build.gradle file to get rid of error

---
+++ app/src/main/java/cs340/pollerexpress/SetupFacade.java
@@ -0,0 +1,20 @@
+package cs340.pollerexpress;
+
+import com.pollerexpress.models.LoginResponse;
+
+public class SetupFacade {
+
+    /**
+     *
+     * @param userName
+     * @param password
+     * @return
+     */
+    LoginResponse login(String userName, String password){
+        return null;
+    }
+
+    LoginResponse register(String userName, String password){
+        return null;
+    }
+}
