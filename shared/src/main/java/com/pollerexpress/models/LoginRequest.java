89f9baabacf817364d62130d19a0748c2c679955
nate <natehebbert@gmail.com>
2018-10-03 18:38:48 -0600

    modified .gitignore and added a default constructor and setters on loginRequest (for spring to deserialize)

--- shared/src/main/java/com/pollerexpress/models/LoginRequest.java
+++ shared/src/main/java/com/pollerexpress/models/LoginRequest.java
@@ -4,6 +4,9 @@ public class LoginRequest {
 	public String username;
 	public String password;
 
+	public LoginRequest() {
+	}
+
 	public LoginRequest(String username, String password) {
 		this.username = username;
 		this.password = password;
@@ -12,4 +15,12 @@ public class LoginRequest {
 	public String getUsername() { return username; }
 
 	public String getPassword() { return password; }
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
