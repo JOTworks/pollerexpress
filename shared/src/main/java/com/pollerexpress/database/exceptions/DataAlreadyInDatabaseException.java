ac683a5bddb0057de4658f6d3d096172a5afcb18
Torsten Gang <xeonocide@gmail.com>
2018-10-07 10:41:15 -0600

    create game added

---
+++ shared/src/main/java/com/pollerexpress/database/exceptions/DataAlreadyInDatabaseException.java
@@ -0,0 +1,9 @@
+package pollerexpress.database.exceptions;
+
+public class DataAlreadyInDatabaseException extends DatabaseException
+{
+    public DataAlreadyInDatabaseException(String data, String target)
+    {
+        super(String.format("%s is already in %s", data, target));
+    }
+}
