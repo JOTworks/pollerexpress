027dac635df750e2da799b2a843c55a95dc64bba
boekwega <boekweg@gmail.com>
2018-10-08 09:42:18 -0600

    game selection presenter

--- app/src/main/java/presenter/GameSelectionPresenter.java
+++ app/src/main/java/presenter/GameSelectionPresenter.java
@@ -1,8 +1,13 @@
 package presenter;
 
+import java.util.List;
+
 import Views.IGameSelectionView;
 import cs340.pollerexpress.SetupFacade;
 
+/**
+ * Responsible for implementing logic for gameselectionview . . . I think
+ */
 public class GameSelectionPresenter implements IGameSelectionPresenter {
 
     private IGameSelectionView view;
@@ -14,7 +19,7 @@ public class GameSelectionPresenter implements IGameSelectionPresenter {
     }
 
     /**
-     * This method contains the logic for what happens when the
+     * (DONE!) This method contains the logic for what happens when the
      * user clickes "create game"
      * @ the user should go the the createGame view
      */
@@ -31,22 +36,39 @@ public class GameSelectionPresenter implements IGameSelectionPresenter {
      * @post the user should go the game lobby
      */
     @Override
-    public void joinGame() {
-
+    public void joinGame(String gameID) {
 
         view.changeLobbyView();
 
         //update the number of players in that game
-        updatePlayerNumber();
+        updatePlayerNumber(gameID);
     }
+
+    /** Gets the list of games for the view to display */
+    @Override
+    public List getGameList() {
+
+        // if not from the setupFacade, where would the
+        // presenter get the list of games?
+        return facade.getGameList();
+
+        /* Client data has a "getGameInfoList" method,
+        but I believe that just returns a list of games
+        that the user is already in, not all of the games
+        that it's possible for the user to join.
+         */
+    }
+
 
     /**
      * This method updates the number of players in a game
      */
-    private void updatePlayerNumber() {
+    private void updatePlayerNumber(String gameID) {
 
         //I'm not quite sure how to do this.
         //I need to look at the UML and figure out how to
         //access and update a particular game (or gameinfo)
+
+
     }
 }
--- app/src/main/java/presenter/IGameSelectionPresenter.java
+++ app/src/main/java/presenter/IGameSelectionPresenter.java
@@ -7,7 +7,12 @@ package presenter;
  */
 public interface IGameSelectionPresenter {
 
+    /** Logic for when "create game" button is clicked */
     public void createGame();
 
+    /** Logic for when "join game" button is clicked */
     public void joinGame();
+
+    /** Gets the list of games for the view to display */
+    public void getGameList();
 }
