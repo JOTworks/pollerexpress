b3050273ae505bbd173d41cc9ed11f62e6bb1f80
jack <2jacksonhunt@gmail.com>
2018-10-08 20:04:52 -0600

    lobbyfrag wip

--- app/src/main/java/Views/LobbyFragment.java
+++ app/src/main/java/Views/LobbyFragment.java
@@ -1,4 +1,90 @@
 package Views;
 
-public class LobbyFragment implements ILobbyView {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.pollerexpress.models.GameInfo;
+
+import Views.recycleViewAdapters.GameSelectAdapter;
+import cs340.pollerexpress.R;
+import presenter.GameSelectionPresenter;
+import presenter.IGameSelectionPresenter;
+import presenter.ILobbyPresenter;
+import presenter.LobbyPresenter;
+import presenter.LoginPresenter;
+
+public class LobbyFragment extends Fragment implements ILobbyView {
+
+    private ILobbyPresenter lobbyPresenter;
+
+    private RecyclerView mGameRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private Button startGameButton;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        lobbyPresenter = new LobbyPresenter(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_game_selection, container, false);
+
+
+        mGameRecyclerView = (RecyclerView) v.findViewById(R.id.game_selection_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mGameRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mGameRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new GameSelectAdapter(new GameInfo[] {
+                new GameInfo("id", "game1", 5, 2),
+                new GameInfo("id", "game2", 4, 1)}, gameSelectionPresenter); //TODO: use getGameList()
+        mGameRecyclerView.setAdapter(mAdapter);
+
+
+        //---------------------Create Game Button---------------------------------------------------
+        startGameButton = (Button) v.findViewById(R.id.start_game_button);
+
+        Button mStartGameButton = (Button) startGameButton;
+        mStartGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: pull this to a method and give control to the presenter
+                FragmentManager fragmentManager = getFragmentManager();
+                //Fragment startGameFragment = fragmentManager.findFragmentById(R.id.fragment_create_game);
+                Fragment GameFragment = new GameFragment();
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.right_side_fragment_container, GameFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return v;
+    }
+
+
+
 }
