d76700367b2c155b67dece740b2b78756c76fc80
Torsten Gang <xeonocide@gmail.com>
2018-10-09 01:06:16 -0600

    poll handler added

--- server/src/main/java/com/pollerexpress/server/Server.java
+++ server/src/main/java/com/pollerexpress/server/Server.java
@@ -1,6 +1,5 @@
-package com.Server;
+package com.pollerexpress.server;
 
-import com.pollerexpress.server.ExecuteHandler;
 import com.sun.net.httpserver.*;
 import java.io.*;
 import java.net.*;
@@ -36,8 +35,8 @@ public class Server
 
         //server.createContext("/login", new LoginHandler());
         //server.createContext("/register", new RegisterHandler());
-       // server.createContext("/exec", new ExecuteHandler());
-
+        server.createContext("/exec", new ExecuteHandler());
+        server.createContext("/poll", new PollHandler());
         System.out.println("Starting Server");
         server.start();
 
@@ -49,7 +48,15 @@ public class Server
     // "args" should contain one command-line argument, which is the port number
     // on which the Server should accept incoming client connections.
     public static void main(String[] args) {
-        String portNumber = args[0];
+        String portNumber;
+        if(args.length > 0)
+        {
+            portNumber = args[0];
+        }
+        else
+        {
+            portNumber = "8080";
+        }
         new Server().run(portNumber);
     }
 }
