e4d993fe37e0a2133f6815ec99dd95db5ad3885f
Morgan Hunt <to.morgan.hunt@gmail.com>
2018-10-28 17:37:59 -0600

    set up TestChat.java on client side

---
+++ app/src/test/java/thePollerExpress/communication/TestChat.java
@@ -0,0 +1,27 @@
+package thePollerExpress.communication;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestChat {
+
+    @Before
+    public void up() {
+        //setup test
+    }
+
+    @After
+    public void down() {
+        //takedown test
+    }
+
+    @Test
+    public void test() {
+        assertTrue(true);
+        assertEquals(1,1);
+        assertNotNull("hi");
+    }
+}
