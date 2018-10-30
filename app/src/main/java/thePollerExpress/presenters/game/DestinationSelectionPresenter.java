bef6a582f568e989400733bbab7b983a06d471a4
boekwega <boekweg@gmail.com>
2018-10-29 22:31:35 -0600

    DestinationSelectionPresenter

--- app/src/main/java/thePollerExpress/presenters/game/DestinationSelectionPresenter.java
+++ app/src/main/java/thePollerExpress/presenters/game/DestinationSelectionPresenter.java
@@ -1,38 +1,58 @@
 package thePollerExpress.presenters.game;
 
+import com.shared.exceptions.CommandFailed;
 import com.shared.models.Command;
+import com.shared.models.DestinationCard;
 import com.shared.models.User;
+import com.shared.models.interfaces.ICommand;
 import com.shared.utilities.CommandsExtensions;
+
+import java.io.ObjectInput;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
 import thePollerExpress.facades.GameFacade;
 import thePollerExpress.models.ClientData;
+import thePollerExpress.presenters.game.interfaces.IDestinationSelectionPresenter;
 import thePollerExpress.presenters.game.interfaces.IGamePresenter;
 import thePollerExpress.utilities.AsyncRunner;
+import thePollerExpress.views.game.interfaces.IDestinationSelectionView;
 import thePollerExpress.views.game.interfaces.IGameView;
 
 /**
  * Abby
  */
-public class DestinationSelectionPresenter implements IGamePresenter {
+public class DestinationSelectionPresenter implements IDestinationSelectionPresenter, Observer {
 
-    private IGameView view;
+    private IDestinationSelectionView view;
     private GameFacade facade;
-    ClientData clientData = ClientData.getInstance();
+    ClientData CD = ClientData.getInstance();
+
+    public DestinationSelectionPresenter(IDestinationSelectionView view) {
+        this.view = view;
+        CD.getUser().getDestCardHand().addObserver(this);
+
+    }
 
     @Override
-    public void discardDestCard(int destCardIndex) {
+    public void discardDestCard(final DestinationCard card) {
         //update = false;
-        User user = clientData.getUser();
+        AsyncRunner discardDestCardTask = new AsyncRunner(view);
 
-        AsyncRunner commandRunner = new AsyncRunner(view);
-
-        Class<?>[] types = {User.class, Integer.class};
-        Object[] params = {user, destCardIndex};
-        Command discardDestCard = new Command(CommandsExtensions.clientSideFacade + "GameFacade",
-                "discardDestCard", types, params, new GameFacade());
-
-        commandRunner.execute(discardDestCard);
+        discardDestCardTask.execute(new ICommand()
+        {
+            @Override
+            public Object execute() throws CommandFailed
+            {
+                return facade.discardDestCard(CD.getUser(), card);
+            }
+        });
         //update = true;
     }
 
+    @Override
+    public void update(Observable o, Object arg) {
+        view.renderCards((List<DestinationCard>) arg);
+    }
 }
