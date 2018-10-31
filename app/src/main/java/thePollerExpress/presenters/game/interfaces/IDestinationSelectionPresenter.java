591613e8f48dc132c9e17333d9fbec0eb5eea244
AbbyB <boekwega@gmail.com>
2018-10-31 14:43:00 -0600

    added comments

--- app/src/main/java/thePollerExpress/presenters/game/interfaces/IDestinationSelectionPresenter.java
+++ app/src/main/java/thePollerExpress/presenters/game/interfaces/IDestinationSelectionPresenter.java
@@ -5,4 +5,15 @@ import com.shared.models.DestinationCard;
 public interface IDestinationSelectionPresenter {
 
     void discardDestCard(DestinationCard card);
+
+    /*
+    * "The following items should be demonstrated:
+    * . add/remove player destination cards for this player
+    * . update the number of destination cards for opponent players
+    * . update the number of cards in destination card deck" -- specs
+    *
+    * "Each players receives 3 destination cards to start the game: 10 points
+    * Each player returns up to 1 destination card to the server: 10 points"
+    * -- implementation rubric */
+
 }
