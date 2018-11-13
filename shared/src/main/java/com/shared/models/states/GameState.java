a3163630387463284f435ba97fde9fcca3886c53
jackson <2jacksonhunt@gmail.com>
2018-11-13 16:51:50 -0700

    added gameState and woking on bankstatefullnes

--- shared/src/main/java/com/shared/models/states/GameState.java
+++ shared/src/main/java/com/shared/models/states/GameState.java
@@ -1,3 +1,19 @@
 package com.shared.models.states;
 
-public class GameState { }
+public class GameState {
+
+    String activePlayer;
+    SUBSTATE subState;
+
+   public enum SUBSTATE
+   {
+       //before first turn states
+       NEED_DISCARD_5, NEED_DISCARD_4, NEED_DISCARD_3, NEED_DISCARD_2, NEED_DISCARD_1,
+       //player turn states
+       DRAWN_ONE, TURN_BEGINNING, DRAWN_DEST,
+       //after last turn state
+       GAME_OVER
+   }
+}
+
+
