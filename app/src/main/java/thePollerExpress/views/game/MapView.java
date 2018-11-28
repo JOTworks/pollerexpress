64837de0ac63b7c93dc81b8f25dd2262396b336c
nate <natehebbert@gmail.com>
2018-11-27 21:15:55 -0700

    changed the map

--- app/src/main/java/thePollerExpress/views/game/MapView.java
+++ app/src/main/java/thePollerExpress/views/game/MapView.java
@@ -87,13 +87,13 @@ public class MapView extends Fragment implements IMapView, IPollerExpressView
         float min_distance = 18;//TODO make this a constant
         Route chosen = null;
         //displayError(String.valueOf(vScroll.getScrollY()));
-
+        float x = map.x - vScroll.getX();//+ hScroll.getScrollX()
+        float y = map.y + vScroll.getScrollY() - vScroll.getY();
         for(Route r : myMap.getRoutes())
         {
             //find the distance
             AnchorPoints pts = new AnchorPoints(r);
-            float x = map.x - vScroll.getX();//+ hScroll.getScrollX()
-            float y = map.y + vScroll.getScrollY() - vScroll.getY();
+
             float distance = pts.aprox(x, y);
             //Log.d("drawView Click", String.format("%d, %d", hScroll.getScrollX(), vScroll.getScrollY()));
             //Log.d("drawViewListener",String.format("distance = %f, p(%f, %f)%s, %s", distance, x, y, pts.toString(), r.toString()) );
