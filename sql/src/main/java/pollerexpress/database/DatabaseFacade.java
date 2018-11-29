a2a9ee12c306a79aeb0b6694748c2805ac55042d
Morgan Hunt <to.morgan.hunt@gmail.com>
2018-11-28 21:04:23 -0700

    fixed but in database facade (again)

--- sql/src/main/java/pollerexpress/database/DatabaseFacade.java
+++ sql/src/main/java/pollerexpress/database/DatabaseFacade.java
@@ -218,6 +218,7 @@ public class DatabaseFacade implements IDatabaseFacade
         {
             db.open();
             Game game = db.getGameDao().read( info.getId() );
+            db.close(false);
             return game;
         }
         catch(DatabaseException e)
@@ -226,7 +227,7 @@ public class DatabaseFacade implements IDatabaseFacade
         }
         finally
         {
-            db.close(false);
+            if(db.isOpen()){db.close(false);}
         }
     }
 
@@ -591,8 +592,8 @@ public class DatabaseFacade implements IDatabaseFacade
     @Override
     public void setupPlayers(GameInfo game) throws DatabaseException {
         try {
-            db.open();
             Player[] players = getPlayersInGame(game);
+            db.open();
             for (Player p : players) {
                 db.getUserDao().setPlayerPoints(p, 0);
                 db.getUserDao().setPlayerTrainCars(p, 40);
