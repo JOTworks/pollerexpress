093f6f35f2f1fceb493de380963f26fcad496c65
boekwega <boekweg@gmail.com>
2018-12-10 09:27:50 -0700

    fixed typo

--- server/src/main/java/com/thePollerServer/utilities/PersistenceProvider.java
+++ server/src/main/java/com/thePollerServer/utilities/PersistenceProvider.java
@@ -7,36 +7,36 @@ import com.shared.models.Player;
 
 import java.util.ArrayList;
 
-public class PersistanceProvider {
+public class PersistenceProvider {
 
     private int delta;
 
-    public PersistanceProvider(int delta) {
+    public PersistenceProvider(int delta) {
         this.delta = delta;
     }
 
     public void register(Player player) {
-        throw new NotImplementedException("register");
+        throw new NotImplementedException("PersistenceProvider::register");
     }
 
     public void saveGame(Game game) {
-        throw new NotImplementedException("register");
+        throw new NotImplementedException("PersistenceProvider::saveGame");
     }
 
     public void getUserList() {
-        throw new NotImplementedException("register");
+        throw new NotImplementedException("PersistenceProvider::getUserList");
     }
 
     public void joinGame(Player player, Game game) {
-        throw new NotImplementedException("register");
+        throw new NotImplementedException("PersistenceProvider::joinGame");
     }
 
     public void addGame(Game game) {
-        throw new NotImplementedException("register");
+        throw new NotImplementedException("PersistenceProvider::addGame");
     }
 
     public void getGameList() {
-        throw new NotImplementedException("register");
+        throw new NotImplementedException("PersistenceProvider::getGameList");
     }
 
     /**
@@ -44,14 +44,14 @@ public class PersistanceProvider {
      * @return all commands that have yet to be saved for this game
      */
     public ArrayList getCommandList(Game game) {
-        throw new NotImplementedException("register");
+        throw new NotImplementedException("PersistenceProvider::getCommandList");
     }
 
     public void addCommand(Command command) {
-        throw new NotImplementedException("register");
+        throw new NotImplementedException("PersistenceProvider::addCommand");
     }
 
     public void onServerStart() {
-        throw new NotImplementedException("register");
+        throw new NotImplementedException("PersistenceProvider::onServerStart");
     }
 }
