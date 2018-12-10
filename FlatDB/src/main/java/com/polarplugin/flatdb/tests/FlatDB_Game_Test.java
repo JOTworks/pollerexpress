be7bed02841a0c646f16c17a0e5eaa98297c6ecd
nate <natehebbert@gmail.com>
2018-12-10 16:41:27 -0700

    fix tests in flatDao

--- FlatDB/src/main/java/com/polarplugin/flatdb/tests/FlatDB_Game_Test.java
+++ FlatDB/src/main/java/com/polarplugin/flatdb/tests/FlatDB_Game_Test.java
@@ -1,11 +1,9 @@
 package com.polarplugin.flatdb.tests;
 
-import com.polarplugin.flatdb.dao.FlatCommandDao;
+import com.plugin.models.ServerGame;
 import com.polarplugin.flatdb.dao.FlatGameDao;
 import com.polarplugin.flatdb.exceptions.DeleteFailedException;
 import com.polarplugin.flatdb.exceptions.GameNotFoundException;
-import com.shared.models.Command;
-import com.shared.models.Game;
 import com.shared.models.GameInfo;
 
 import org.junit.Assert;
@@ -13,16 +11,15 @@ import org.junit.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
 
 public class FlatDB_Game_Test {
 
     @Test
     public void testFileWriting() throws IOException {
         FlatGameDao fgd = new FlatGameDao();
-        Game myGame = new Game(new GameInfo("myGameId", "myGame", 2));
+        ServerGame myGame = new ServerGame(new GameInfo("myGameId", "myGame", 2));
         fgd.addGame(myGame);
-        Game myReadGame = fgd.getGame("myGameId");
+        ServerGame myReadGame = fgd.getGame("myGameId");
         Assert.assertTrue(myReadGame.getId().equals("myGameId"));
     }
 
@@ -30,11 +27,11 @@ public class FlatDB_Game_Test {
     public void testDeleteGame() throws IOException {
             FlatGameDao fgd = new FlatGameDao();
             fgd.clearGames();
-            Game myGame = new Game(new GameInfo("myGameId", "myGame", 2));
+            ServerGame myGame = new ServerGame(new GameInfo("myGameId", "myGame", 2));
             fgd.addGame(myGame);
             fgd.deleteGame(myGame);
             try {
-                Game myReadGame = fgd.getGame("myGameId");
+                ServerGame myReadGame = fgd.getGame("myGameId");
                 // fail if we don't throw
                 Assert.assertTrue(false);
             } catch (GameNotFoundException e) {
@@ -48,8 +45,8 @@ public class FlatDB_Game_Test {
 
         FlatGameDao fgd = new FlatGameDao();
         try { fgd.clearGames(); } catch (Exception e) {};
-        Game myGame = new Game(new GameInfo("myGameId", "myGame", 2));
-        Game myGame2 = new Game(new GameInfo("myGameId2", "myGame", 2));
+        ServerGame myGame = new ServerGame(new GameInfo("myGameId", "myGame", 2));
+        ServerGame myGame2 = new ServerGame(new GameInfo("myGameId2", "myGame", 2));
         fgd.addGame(myGame);
         try {
             fgd.deleteGame(myGame2);
@@ -64,13 +61,13 @@ public class FlatDB_Game_Test {
         boolean testPassed = false;
 
         FlatGameDao fgd = new FlatGameDao();
-        Game myGame = new Game(new GameInfo("myGameId", "myGame", 2));
+        ServerGame myGame = new ServerGame(new GameInfo("myGameId", "myGame", 2));
         fgd.addGame(myGame);
         fgd.addGame(myGame);
         fgd.addGame(myGame);
         fgd.clearGames();
         try {
-            Game myReadGame = fgd.getGame("myGameId");
+            ServerGame myReadGame = fgd.getGame("myGameId");
         } catch (FileNotFoundException e) {
             testPassed = true;
         }
